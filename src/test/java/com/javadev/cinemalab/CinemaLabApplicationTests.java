811876702216e6885851232787debb5a84ff18da
sherzataziz <xirzat_112@hotmail.com>
2022-07-18 19:29:16 -0700

    Initial commit

---
+++ src/test/java/com/javadev/cinemalab/CinemaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.cinemalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
